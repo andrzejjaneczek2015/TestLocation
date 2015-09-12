e2279d51e3a9caf8e9004dacff3c88dcc719ecfd
Andrzej Janeczek <andrzej.janeczek@gmail.com>
2015-09-12 20:46:54 +0200

    werwerwerwer

--- Andrzej/MyApplication/app/src/main/java/encomit/eu/myapplication/MainActivity.java
+++ Andrzej/MyApplication/app/src/main/java/encomit/eu/myapplication/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // as you specify a parent activity in AndroidManifest.xml. hhhfgfuurrr
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
