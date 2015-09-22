b5a4d962ae1310997fb3e970a28743b98e95b34a
Andrzej Janeczek <andrzej.janeczek@gmail.com>
2015-09-22 10:39:03 +0200

    start

--- TestServices/app/src/main/java/encomit/eu/testservices/services/MyOwnServices.java
+++ TestServices/app/src/main/java/encomit/eu/testservices/services/MyOwnServices.java
@@ -1,10 +1,33 @@
 package encomit.eu.testservices.services;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
 /**
  * Created by Andrzej Janeczek on 2015-09-22.
  */
-public class MyOwnServices {
+public class MyOwnServices  extends Service{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    // Odpowiedzialne za polącznie z Activity
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
