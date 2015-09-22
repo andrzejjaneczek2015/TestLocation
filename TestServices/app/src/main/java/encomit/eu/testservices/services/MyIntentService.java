95740b6e955e04de95632d447caaf3d78ab167e5
Andrzej Janeczek <andrzej.janeczek@gmail.com>
2015-09-22 11:06:52 +0200

    start

---
+++ TestServices/app/src/main/java/encomit/eu/testservices/services/MyIntentService.java
@@ -0,0 +1,7 @@
+package encomit.eu.testservices.services;
+
+/**
+ * Created by Andrzej Janeczek on 2015-09-22.
+ */
+public class MyIntentService {
+}
--- TestServices/app/src/main/java/encomit/eu/testservices/services/MyOwnServices.java
+++ TestServices/app/src/main/java/encomit/eu/testservices/services/MyOwnServices.java
@@ -2,26 +2,39 @@ package encomit.eu.testservices.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Andrzej Janeczek on 2015-09-22.
  */
-public class MyOwnServices  extends Service{
+public class MyOwnServices extends Service {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Toast.makeText(MyOwnServices.this, "Start service.onCreate", Toast.LENGTH_SHORT).show();
     }
 
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        Toast.makeText(MyOwnServices.this, "Start service.onStartCommand", Toast.LENGTH_SHORT).show();
+
+
+        // Uruchomienie taska
+        new ServicesTask().execute();
+
+        // Service zakończy się po jawnym wyłączeniu - asynctasku stopSelf();
+        return START_NOT_STICKY;
     }
 
     @Override
     public void onDestroy() {
+        Toast.makeText(getApplicationContext(), "OnDestroy Service ", Toast.LENGTH_SHORT).show();
         super.onDestroy();
     }
 
@@ -30,4 +43,30 @@ public class MyOwnServices  extends Service{
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+
+
+    // AsyncTask ..
+    private class ServicesTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                Log.e(getClass().getName(), "Zablokowany watek");
+
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(getApplicationContext(), "Zakończyłem działanie taska", Toast.LENGTH_SHORT).show();
+            // zakończenie działania services
+            stopSelf();
+        }
+    }
+
+
 }
